df221b66559e02c3e29161873453cc66aea2f6b7
Ashwini <ashwiniparaye9309@gmail.com>
2023-04-04 16:20:47 +0530

    reduce

--- index.js
+++ index.js
@@ -729,11 +729,23 @@
 
 //!          5.   filter
 
-let a = [1, 2, 3, 4];
+// let a = [1, 2, 3, 4];
 
-let a2 = a.filter((value) => {
-  return value >= 3;
+// let a2 = a.filter((value) => {
+//   return value >= 3;
+// });
+
+// console.log("a2==== ", a2);
+// console.log("a==== ", a);
+
+///////////////////////////////////////////////////////////////////////
+
+//!          5.   reduce
+
+let a = [1, 2, 3, 4, 5];
+
+let a2 = a.reduce((val1, val2) => {
+  return val1 + val2;
 });
 
-console.log("a2==== ", a2);
-console.log("a==== ", a);
+console.log("a2 ======== ", a2);
