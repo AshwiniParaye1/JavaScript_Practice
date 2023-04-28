e769460a59c2a39e8e4d6d7290258082259e684d
Ashwini <ashwiniparaye9309@gmail.com>
2023-04-28 18:50:24 +0530

    hoisting

--- index.js
+++ index.js
@@ -1000,3 +1000,20 @@
 // }
 
 // z();
+
+////////////////////////////////////////////////////////////////////////
+
+//!                setTimeout and closures
+
+function x() {
+  var i = 1;
+
+  for (var i = 1; i <= 5; i++) {
+    setTimeout(function () {
+      console.log(i);
+    }, i * 1000);
+  }
+
+  console.log("Hellooo!!! outside setTimeout");
+}
+x();
